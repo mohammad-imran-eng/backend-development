379f9ced46b8ee08533ac54521d434cda4ff24cd
Mohammad Imran <immu.engineer@gmail.com>
2025-08-30 14:13:30 +0600

    practice api

--- express/express.js
+++ express/express.js
@@ -2,8 +2,44 @@ const express = require('express');
 const app = express();
 port = 3000;
 
+
+app.use(express.json());
+
+// dummy json data
+const newsData = {
+    newsTitle: "Sheikh hasina palaise",
+    newsViews: "10 Million"
+}
+
 app.get('/',(req,res)=> {
     res.send("Wecome to express server");
+})
+
+app.get('/news',(req,res)=> {
+    res.send(newsData);
+})
+
+app.post('/login',(req,res)=> {
+    console.log(req.body);
+    res.send(
+        {
+            statue:2
+            ,message: "Welcome to post method",
+            data:req.body,
+            queryData: req.query
+        });
+})
+
+app.get('/news/:id',(req,res)=> {
+    const id = req.params.id;
+    console.log(id);
+    res.send(id);
+})
+
+app.get('/about',(req,res)=> {
+    const data = req.query;
+    console.log(data);
+    res.send(data);
 })
 
 app.listen(port,()=> {
