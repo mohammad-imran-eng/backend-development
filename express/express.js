fef1490ca6332d1b89a16e1cb8c46eedada88855
Mohammad Imran <immu.engineer@gmail.com>
2025-08-30 15:26:54 +0600

    practice express middleware

--- express/express.js
+++ express/express.js
@@ -5,6 +5,35 @@ port = 3000;
 
 app.use(express.json());
 
+const myToken ="1234"
+
+const checkToken = (req,res,next)=> {
+    if(req.query.token === myToken){
+         next()
+    }
+    else {
+       res.status(404).send({message:"unathorized access"});
+    }
+    
+    
+}
+
+const myPass = "24680"
+
+
+// custom middleware
+app.use(checkToken);
+app.use((req,res,next)=> {
+    console.log(req.query.pass)
+    if(req.query.pass == myPass){
+        ;
+        next();
+    }
+    else {
+        res.status(404).send({message: "password was invalid"});
+    }
+})
+
 // dummy json data
 const newsData = {
     newsTitle: "Sheikh hasina palaise",
@@ -41,6 +70,8 @@ app.get('/about',(req,res)=> {
     console.log(data);
     res.send(data);
 })
+
+
 
 app.listen(port,()=> {
     console.log("Server is running")
